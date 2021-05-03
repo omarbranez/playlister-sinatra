f732d77e9b9ea94fe35e01b440957ac5b2ea95e6
Omar Branez <omar.branez@gmail.com>
2021-05-02 21:39:42 -0400

    commit tree ccd3705a6a5c2a7adbf9bd92c5b092b0629f0cb4

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,29 +9,32 @@ class SongsController < ApplicationController
     end
 
     post '/songs' do
+        # binding.pry # used to figure out we need to grab the genre_ids, not the genre name
         @song = Song.create(params[:song])
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
         @song.genre_ids = params[:genres]
         @song.save
-        redirect "/songs/#{@song.slug}"
+        redirect to "/songs/#{@song.slug}"
     end
 
     get '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
+        # binding.pry # used to show that song.genres is nested, plus we have to account for multiple genres (classic alternative)
         # find_by_slug, because find defaults to id
         erb :'songs/show'
     end
 
     get '/songs/:slug/edit' do
-        @song = Song.find(params[:slug])
+        @song = Song.find_by_slug(params[:slug])
         erb :'songs/edit'
     end
 
     patch '/songs/:slug' do
-        @song = Song.find(params[:slug])
-        #binding.pry
+        # binding.pry # used to show where the artist is in params
+        @song = Song.find_by_slug(params[:slug])
         @song.update(params[:song])
         @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genre_ids = params[:genres]
         @song.save
         redirect to "/songs/#{@song.slug}"
     end
--- config.ru
+++ config.ru
@@ -3,8 +3,11 @@ require './config/environment'
 begin
   fi_check_migration
 
-  use Rack::SongController
   use Rack::MethodOverride
+  use GenresController
+  use ArtistsController
+  use SongsController
+  # add each controller as you write out their routes, or sinatra wont know they exist
   run ApplicationController
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
